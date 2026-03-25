4dedb95d5aa3deb1f91236f72f4249835fced8c1
maxwe-exe <max591b@gmail.com>
2026-03-24 22:11:36 -0700

    added basic movement function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(Startup, placeholder)
         .add_systems(Startup, add_plane)
+        .add_systems(Update, user_input)
         .run();
 }
 
@@ -36,6 +37,7 @@ fn placeholder(
 ){
         // cube
     commands.spawn((
+        MantisPhysics{ speed: 0.1 },
         Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
         MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
         Transform::from_xyz(0.0, 0.5, 0.0),
@@ -47,10 +49,44 @@ fn add_plane(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ){
-// circular base
+
     commands.spawn((
         Mesh3d(meshes.add(Circle::new(4.0))),
         MeshMaterial3d(materials.add(Color::WHITE)),
         Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
     ));
+}
+
+
+fn user_input(
+    mut mantis: Single<(&mut Transform, &MantisPhysics)>,
+    input: Res<ButtonInput<KeyCode>>,
+){  
+    let mut transform = (0.0, 0.0);
+    if input.pressed(KeyCode::KeyW) {
+        transform.1 += 1.0;
+
+    }
+    else if input.pressed(KeyCode::KeyS) {
+
+        transform.1 -= 1.0;
+    }
+    if input.pressed(KeyCode::KeyA) {
+        transform.0 -= 1.0;
+
+    }
+    else if input.pressed(KeyCode::KeyD) {
+        transform.0 += 1.0;
+    }
+    let speed = mantis.1.speed;
+    mantis.0.translation.x += transform.0 * speed;
+    mantis.0.translation.z += transform.1 * speed;
+    
+}
+
+
+#[derive(Component)]
+struct MantisPhysics{
+    speed: f32,
+
 }
